d314fe97542ea62345df6a57e883bded58b846d0
Aaro Varis <aaro@aarovaris.com>
2022-09-29 16:04:40 +0300

    .

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ const path = require("path")
 const {ipcMain} = require("electron")
 const writeFileSync = require('fs').writeFileSync;
 const mkdirSync = require('fs').mkdirSync;
-
 module.exports = class moai {
     constructor(env) {
-        // Define plugin enviornment within the class
         this.env = env
         this.playing = null;
     }
-
     onReady(win) {
         if (!existsSync(`${this.env.dir}/dist`)) {
             mkdirSync(`${this.env.dir}/dist`);
@@ -18,17 +15,12 @@ module.exports = class moai {
             try {
                 writeFileSync(`${this.env.dir}/dist/data.txt`, JSON.stringify(attributes))
             } catch(e) {
-                console.log("[Plugin][AaroProg][Error]",e)
+                console.log("[Error]",e)
             }
         })
-
-        console.log("[Plugin][AaroProg] Taskprog Backend Ready.")
     }
-
-    // Called when the renderer is ready (app.init())
     onRendererReady(win) {
         console.debug("Renderer Ready Called")
-        // Load the frontend plugin
         this.env.utils.loadJSFrontend(path.join(this.env.dir, "index.frontend.js"))
     }
 }
